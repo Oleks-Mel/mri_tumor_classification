f94a26fe7f7d9bc2184088fe3a261a6a9c7c3367
Oleks-Mel <80025493+Oleks-Mel@users.noreply.github.com>
2024-03-01 15:59:58 -0500

    Semifinal update to the project

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     seed=000,
     color_mode='grayscale',
     image_size=(img_height, img_width),
-    batch_size = 2)
+    batch_size = batch_size)
 
 print("Val Size:", val_ds.cardinality().numpy())
 
@@ -52,13 +52,14 @@ model = tf.keras.Sequential([
 
 model.compile(
     optimizer='adam',
+    
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['accuracy'])
 
 model.fit(
     x = train_ds,
     validation_data=val_ds,
-    epochs=1
+    epochs=10
 )
 
 class_names = ["Glioma", "Meningioma", "No Tumor", "Pituitary"]
@@ -80,38 +81,51 @@ print('Predictions len:', len(predictions))
 
 class_names_short = ['G', 'M', 'N', 'P']
 
+correct_count = 0
+predictions.flatten()
+label_list = []
+
+for images, labels in val_ds:
+    label_list.extend(labels.numpy().tolist())
+
+
+for i in range(len(label_list)):
+    if(np.argmax(predictions[i]) == label_list[i]):
+        correct_count += 1
+
+
+print("Correct Count:", correct_count)
+
+
 j = 0
 color = ['red', 'red', 'red', 'red']
 same_count = 0
 x = 0
-for a in range(len(predictions)):
-    print(np.argmax(predictions[a]))
-    print(labels[a])
 
-for images, labels in val_ds.take(40):
-    print(class_names[labels[j]])
-    print(class_names[np.argmax(predictions[j])])
-    print('\n')
-    if labels[j] == np.argmax(predictions[j]):
-        same_count += 1
-    # figure, (axis1, axis2) = plt.subplots(1, 2)
-    # plt.title(class_names[labels[j]])
-    # axis1.imshow(images[j].numpy())
-    # #print(class_names[labels[0]])
-    # #print(predictions[0])
-    # index_max = np.argmax(predictions[j])
-    # color[index_max] = 'blue'
-    # axis2.bar(class_names_short, predictions[j], width=0.3, color = color)
-    # plt.xlabel("Prediction")
-    # plt.ylabel("Certainty")
-    # plt.title("Tumor Predictions")
-    # plt.show()
-    j+=1
-    x+=1
-    # for i in range(len(color)):
-    #     color[i] = 'red'
+# for images, labels in val_ds.take(40):
+#     print(class_names[labels[j]])
+#     print(class_names[np.argmax(predictions[j])])
+#     print('\n')
+#     if labels[j] == np.argmax(predictions[j]):
+#         same_count += 1
+#     # figure, (axis1, axis2) = plt.subplots(1, 2)
+#     # plt.title(class_names[labels[j]])
+#     # axis1.imshow(images[j].numpy())
+#     # #print(class_names[labels[0]])
+#     # #print(predictions[0])
+#     # index_max = np.argmax(predictions[j])
+#     # color[index_max] = 'blue'
+#     # axis2.bar(class_names_short, predictions[j], width=0.3, color = color)
+#     # plt.xlabel("Prediction")
+#     # plt.ylabel("Certainty")
+#     # plt.title("Tumor Predictions")
+#     # plt.show()
+#     j+=1
+#     x+=1
+#     # for i in range(len(color)):
+#     #     color[i] = 'red'
 
-print("Same count =", same_count, "/", x)
+# print("Same count =", same_count, "/", x)
 
 # p=0
 # for images, labels in val_ds:
